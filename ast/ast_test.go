11514a23312bece6cad186b9c7ee4905f6833b15
Atte Kojo <atte.kojo@gmail.com>
2026-03-10 16:55:38 +0000

    feat: Add String() for AST nodes
    Add String() methods for AST nodes so that code can be regenerated from the AST

---
+++ ast/ast_test.go
@@ -0,0 +1,26 @@
+package ast
+
+import (
+	"testing"
+
+	"github.com/akojo/monkey/token"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name:  &Identifier{Token: token.Token{Type: token.IDENT, Literal: "var"}, Value: "var"},
+				Value: &Identifier{Token: token.Token{Type: token.IDENT, Literal: "value"}, Value: "value"},
+			},
+		},
+	}
+
+	expected := "let var = value;"
+	got := program.String()
+
+	if got != expected {
+		t.Errorf("program.String(): expected %q, got %q", expected, got)
+	}
+}
