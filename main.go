c22b60f5450bf7212c3f46ea2cf81570f9e4dfef
Atte Kojo <atte.kojo@gmail.com>
2026-04-08 15:16:33 +0300

    feat: Read program optionally from a file

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/akojo/monkey/repl"
@@ -8,9 +9,34 @@ import (
 )
 
 func main() {
+	if len(os.Args) > 2 {
+		fmt.Fprintf(os.Stderr, "Usage: monkey [file | -]\n")
+		os.Exit(1)
+	}
+
+	if len(os.Args) == 2 {
+		runFile(os.Args[1])
+		return
+	}
+
 	if term.IsTerminal(int(os.Stdin.Fd())) {
 		repl.Start(os.Stdin, os.Stdout)
 	} else {
 		repl.Run(os.Stdin, "<stdin>")
 	}
+}
+
+func runFile(filename string) {
+	if filename == "-" {
+		repl.Run(os.Stdin, "<stdin>")
+		return
+	}
+
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+
+	repl.Run(file, filename)
 }
